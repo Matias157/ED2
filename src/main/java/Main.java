1dc50c5c4d6936ad990d208e22726c3388065870
Matias159 <alexandrehmatias@hotmail.com>
2017-10-09 23:12:10 -0300

    Main inserido

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 import utfpr.dainf.ct.ed.exemplo.ArvoreBinaria;
 
-/*
+/**
  * UTFPR - Universidade Tecnológica Federal do Paraná
  * DAINF - Departamento Acadêmico de Informática
  * 
@@ -15,7 +15,10 @@ import utfpr.dainf.ct.ed.exemplo.ArvoreBinaria;
  *               1   6     14 
  *                  / \    /
  *                 4   7  13
+                    \
+                     5
  * 
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
  */
 public class Main {
     public static void main(String[] args) {
@@ -26,39 +29,44 @@ public class Main {
         ArvoreBinaria<Integer> e = a.insereEsquerda(3);
         e.insereEsquerda(1);
         ArvoreBinaria<Integer> d = e.insereDireita(6);
-        d.insereEsquerda(4);
+        d.insereEsquerda(4).insereDireita(5);
         d.insereDireita(7);
 
         // monta o ramo direito
         a.insereDireita(10).insereDireita(14).insereEsquerda(13);
         
-        ArvoreBinaria<Integer> a2 = a;
-        ArvoreBinaria<Integer> a3 = a;
-        ArvoreBinaria<Integer> a4 = a;
-        
-        System.out.println("PERCURSO RECURSIVO POST-ORDER");
-        a.visitaPosOrdem();
-        System.out.println("\nPERCURSO RECURSIVO PRE-ORDER");
-        a.visitaPreOrdem();
-        System.out.println("\nPERCURSO RECURSIVO IN-ORDER");
+        System.out.println("EM ORDEM");
+        System.out.println("PERCURSO RECURSIVO");
         a.visitaEmOrdem();
-        System.out.println("\nPERCURSO ITERATIVO POST-ORDER");
+        System.out.println("\nPERCURSO ITERATIVO");
         ArvoreBinaria<Integer> no;
-        while ((no = a.proximoPosOrdem()) != null) {
-            System.out.print(" " + no.getDado());
-        }
-        System.out.println("\nPERCURSO ITERATIVO PRE-ORDER");
-        while ((no = a.proximoPreOrdem()) != null) {
-            System.out.print(" " + no.getDado());
-        }
-        System.out.println("\nPERCURSO ITERATIVO IN-ORDER");
         while ((no = a.proximoEmOrdem()) != null) {
             System.out.print(" " + no.getDado());
         }
-        System.out.println("\nPERCURSO ITERATIVO IN-LEVEL");
-        while ((no = a.proximoEmNivel()) != null) {
+        
+        a.setInicioTrue();
+        System.out.println("\n\nPRE ORDEM");
+        System.out.println("PERCURSO RECURSIVO");
+        a.visitaPreOrdem();
+        System.out.println("\nPERCURSO ITERATIVO");
+        while ((no = a.proximoPreOrdem()) != null) {
+            System.out.print(" " + no.getDado());
+        }  
+        
+        a.setInicioTrue();
+        System.out.println("\n\nPOS ORDEM");
+        System.out.println("PERCURSO RECURSIVO");
+        a.visitaPosOrdem();
+        System.out.println("\nPERCURSO ITERATIVO");
+        while ((no = a.proximoPosOrdem()) != null) {
             System.out.print(" " + no.getDado());
         }
-        System.out.println(" ");
+        
+        a.setInicioTrue();
+        System.out.println("\n\nEM NIVEL");
+        System.out.println("PERCURSO ITERATIVO");
+        while ((no = a.proximoEmNivel()) != null) {
+            System.out.print(" " + no.getDado());
+        }  
     }
 }
